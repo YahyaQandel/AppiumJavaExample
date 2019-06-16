3cb4ebe71d62e6f76ca7d571b7972c172cb74943
YahyaQandel <yahya.qandel@gmail.com>
2019-06-16 11:33:59 +0200

    update checkusersettings testcase

--- src/test/java/HelloWorldVodafone.java
+++ src/test/java/HelloWorldVodafone.java
@@ -19,17 +19,11 @@ public class HelloWorldVodafone {
     String messagingAppActivityName = "com.google.android.apps.messaging.ui.ConversationListActivity";
     String vodafoneAppPackageName = "com.emeint.android.myservices";
     String vodafoneAppActivityName = "vodafone.vis.engezly.ui.screens.splash.SplashRevampActivity";
-    String vodafoneAppDashboardActivityName = "vodafone.vis.engezly.ui.screens.dashboard.DashboardActivity";
+
     DesiredCapabilities messagingCap ;
     DesiredCapabilities vodafoneCap;
     private static AppiumDriverLocalService service;
 
-    @BeforeClass
-    public static void createAPKS() throws IOException, InterruptedException {
-//      RUN ONLY FIRST TIME TO CREATE THE APKS FOR THE BUNDLE
-//        AndroidHelper.generateApksFromAAB();
-//        AndroidHelper.installAPKSonDevice();
-    }
     @Before
     public void setUp() throws MalformedURLException {
         service = AppiumDriverLocalService.buildDefaultService();
@@ -45,10 +39,35 @@ public class HelloWorldVodafone {
         driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
 
     }
+
+    @Test
+    public void changeUserSettingThroughSms() throws MalformedURLException {
+        loginToMyAccount();
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        openUserInfoPage();
+        openSmsApplication();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        sendMessage();
+        switchToVodafoneApp();
+    }
+
+    private void openUserInfoPage() {
+        driver.findElement(MobileBy.id("desc")).click();
+    }
+
+    @After
+    public void tearDown()  {
+        if (driver != null) {
+            driver.quit();
+        }
+        if (service != null) {
+            service.stop();
+        }
+    }
     private DesiredCapabilities registerCaps(String appPackage,String appActivity){
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("deviceName", "Nexus_5X");
-//        vodafoneCap.setCapability("deviceName", "HUAWEI_P10_lite-65be85067");
+//      fvodafoneCap.setCapability("deviceName", "HUAWEI_P10_lite-65be85067");
         caps.setCapability("platformName", "Android");
         caps.setCapability("appPackage", appPackage);
         caps.setCapability("appActivity", appActivity);
@@ -57,79 +76,30 @@ public class HelloWorldVodafone {
     private void registerVodafoneCaps() {
         vodafoneCap = registerCaps(vodafoneAppPackageName,vodafoneAppActivityName);
     }
-
     private void registerMessagingCaps() {
         messagingCap =  registerCaps(messagingAppPackageName,messagingAppActivityName);
     }
-
-    @Test
-    public void checkHeaderExists() throws MalformedURLException {
-        loginToMyAccount();
-        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-        driver.findElement(MobileBy.id("desc")).click();
-//        driver.findElement(MobileBy.id("toolbar_backIcon")).click();
-//        driver.findElement(MobileBy.id("desc")).click();
-//        driver.findElement(MobileBy.id("ll_main_view")).click();
-//        driver.findElement(MobileBy.id("welcomeView")).click();
-//        driver.findElement(MobileBy.id("negativeBtn")).click();
-//        driver.findElement(MobileBy.id("negativeBtn")).click();
-
-
-//        Activity messagesActivity = new Activity(messagingAppPackageName, messagingAppActivityName);
-//        messagesActivity.setAppWaitPackage(messagingAppPackageName);
-//        messagesActivity.setAppWaitActivity(messagingAppActivityName);
-//        messagesActivity.setStopApp(false);
-//        ((AndroidDriver) driver).startActivity(messagesActivity);
-
-        openSmsApplication();
-        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-//        driver.activateApp("com.google.android.apps.messaging");
-        sendMessage();
-
-        switchToVodafoneApp();
-
-
-        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-        driver.findElement(MobileBy.id("desc")).click();
-
-//        driver.activateApp("com.google.android.apps.messaging", "com.google.android.apps.messaging.ui.ConversationListActivity");
-//        driver.findElement(MobileBy.id("desc")).click();
-//        driver.findElement(MobileBy.id("welcomeView")).click();
-//        driver.findElement(MobileBy.id("ll_main_view")).click();
-//        driver.findElement(MobileBy.id("welcomeView")).click();
-//        driver.findElement(MobileBy.id("negativeBtn")).click();
-//        driver.findElement(MobileBy.id("negativeBtn")).click();
-
-
-    }
-
     private void sendMessage() {
         driver.findElement(MobileBy.id("start_new_conversation_button")).click();
         driver.findElement(MobileBy.id("recipient_text_view")).sendKeys("888");
         driver.findElement(MobileBy.id("contact_picker_create_group")).click();
         driver.findElement(MobileBy.id("compose_message_text")).sendKeys("16555");
         driver.findElement(MobileBy.id("send_message_button_icon")).click();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
     }
-
     private void openSmsApplication() throws MalformedURLException {
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), messagingCap);
     }
-
-    private void switchToVodafoneApp() throws MalformedURLException {
-        Activity vodafoneActivity = new Activity(vodafoneAppPackageName, vodafoneAppActivityName);
-//        vodafoneActivity.setAppWaitPackage(messagingAppPackageName);
-//        vodafoneActivity.setAppWaitActivity(messagingAppActivityName);
-//        vodafoneActivity.setStopApp(false);
-        driver.startActivity(vodafoneActivity);
+    private void switchToVodafoneApp()  {
+        driver.activateApp(vodafoneAppPackageName);
     }
-
     private void loginToMyAccount() {
         driver.findElement(MobileBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.EditText\n")).sendKeys("01090614633");
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
         driver.findElement(MobileBy.id("btnLogin")).click();
-        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
         driver.findElement(MobileBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.EditText")).sendKeys("oNe_status_1");
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
         driver.findElement(MobileBy.id("btnLogin")).click();
     }
 }
